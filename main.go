0776d402a9e861c21f666345c2fecd503bd31f4e
Thakur Prasad Rout <thakurtpr@gmail.com>
2024-05-17 12:08:12 +0530

    changes

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	// "io/ioutil"
 	"log"
 	"net/http"
 	"net/smtp"
 	"strings"
 	"github.com/sethvargo/go-password/password"
-	"github.com/jaswdr/faker/v2"
+	// "github.com/jaswdr/faker/v2"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
@@ -61,7 +61,10 @@ func accessTokenCall() (accessToken interface{}, err error) {
 	defer res.Body.Close()
 
 	var Token_ResponseData map[string]interface{}
-	json.NewDecoder(res.Body).Decode(&Token_ResponseData)
+	err=json.NewDecoder(res.Body).Decode(&Token_ResponseData)
+	if err!=nil{
+		fmt.Println("Error",err)
+	}
 
 	return Token_ResponseData["access_token"].(string), nil
 }
@@ -82,7 +85,10 @@ func getUserId(Username string, accessToken string) (interface{}, error) {
 	var userIdExtractor []map[string]interface{}
 
 	checkUserName:=strings.ToLower(Username)
-	json.NewDecoder(resp.Body).Decode(&userIdExtractor)
+	err=json.NewDecoder(resp.Body).Decode(&userIdExtractor)
+	if err!=nil{
+		fmt.Println("Error",err)
+	}
 	for _, value := range userIdExtractor {
 		if value["username"] == checkUserName {
 			IdUser := value["id"]
@@ -93,18 +99,27 @@ func getUserId(Username string, accessToken string) (interface{}, error) {
 }
 
 func createUserHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
 	Token, err := accessTokenCall()
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println(Token)
+	// fmt.Println(Token)
 
 	var incBodyData User
 
-	fake := faker.New()
-	username:=fake.Person().FirstName()
-	fmt.Println(username)
-	json.NewDecoder(request.Body).Decode(&incBodyData)
+
+	//Username Generator
+	// fake := faker.New()
+	// username:=fake.Person().FirstName()
+	// fmt.Println(username+" User Generated")
+
+
+	
+	err=json.NewDecoder(request.Body).Decode(&incBodyData)
+	if err!=nil{
+		fmt.Println("Error",err)
+	}
 	url := "https://34.93.102.191:18080/auth/admin/realms/camunda-platform/users"
 	method := "POST"
 	dataToSend := fmt.Sprintf(`{
@@ -113,10 +128,11 @@ func createUserHandler(response http.ResponseWriter, request *http.Request) {
 		"email": "%s",
 		"enabled": true,
 		"username": "%s"
-	}`, incBodyData.FirstName, incBodyData.LastName, incBodyData.Email,username)
+	}`, incBodyData.FirstName, incBodyData.LastName, incBodyData.Email,incBodyData.FirstName)
 
 	payload := strings.NewReader(dataToSend)
-	fmt.Println(payload)
+	// fmt.Println(payload)
+
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -128,8 +144,12 @@ func createUserHandler(response http.ResponseWriter, request *http.Request) {
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	respData, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(respData))
+	var responeCreateUser map[string]interface{}
+	err=json.NewDecoder(resp.Body).Decode(&responeCreateUser)
+	if err!=nil{
+		fmt.Println("Error",err)
+	}
+	// fmt.Println(responeCreateUser)
 
 	if resp.StatusCode == 201 {
 		fmt.Println("User created successfully")
@@ -139,16 +159,16 @@ func createUserHandler(response http.ResponseWriter, request *http.Request) {
 		if err!=nil{
 			fmt.Println("Error:",err)
 		}
-		fmt.Println(resPass)
+		// fmt.Println(resPass)
 
 
-		userid, err := getUserId(username, accessToken)
+		userid, err := getUserId(incBodyData.FirstName, accessToken)
 		if err != nil {
 			fmt.Println("Error:", err)
 		}
 
 		userId, ok := userid.(string)
-		fmt.Println(userId)
+		fmt.Println(userId,"Received")
 
 		if !ok {
 			fmt.Println("Error Converting userID")
@@ -169,22 +189,16 @@ func createUserHandler(response http.ResponseWriter, request *http.Request) {
 		accessToken := fmt.Sprintf("Bearer %s", Token)
 		req.Header.Add("Authorization", accessToken)
 		req.Header.Add("Content-Type", "application/json")
-		resp, err := client.Do(req)
+		respPassword, err := client.Do(req)
 		if err != nil {
 			fmt.Println("Error:", err)
 		}
-		if resp.StatusCode == 204 {
+		if respPassword.StatusCode == 204 {
 			fmt.Println("Password set successfully")
 		} else {
+			
 			fmt.Println("Failed to set password")
 		}
-	
-
-		
-
-
-
-
 		url = "https://34.93.102.191:18080/auth/admin/realms/camunda-platform/users/" + userId + "/role-mappings/realm"
 		method = "POST"
 		send= `[
@@ -206,30 +220,37 @@ func createUserHandler(response http.ResponseWriter, request *http.Request) {
 		accessToken = fmt.Sprintf("Bearer %s", Token)
 		req.Header.Add("Authorization", accessToken)
 		req.Header.Add("Content-Type", "application/json")
-		resp, err = client.Do(req)
+		respDatacheck, err := client.Do(req)
 		if err != nil {
 			fmt.Println("Error:", err)
 		}
-		if resp.StatusCode == 204 {
+		if respDatacheck.StatusCode == 204 && respPassword.StatusCode == 204 {
+			//  Send Details To The User
+			auth := smtp.PlainAuth("", "tprop48@gmail.com", "ovgo agtz dsdj bwhq", "smtp.gmail.com")
+			to := []string{incBodyData.Email}
+			msgStr := fmt.Sprintf("To: %s\r\nSubject: Your Details\r\n\r\nID:%s \r\n Password:%s\r\n", incBodyData.Email,incBodyData.FirstName ,resPass)
+			msg := []byte(msgStr)
+			err = smtp.SendMail("smtp.gmail.com:587", auth, "tprop48@gmail.com", to, msg)
+			if err != nil {
+				log.Fatal(err)
+			}
+			json.NewEncoder(response).Encode(map[string]interface{}{
+				"Success": "True",
+				"Message": "Check Mail For Id And Password",
+			})
 			fmt.Println("Role Assigned successfully")
 		} else {
+			json.NewEncoder(response).Encode(map[string]interface{}{
+				"Success": "false",
+				"Message": "User Created But Failed To Assign Role || Password",
+			})
 			fmt.Println("Failed to Assign Role")
 		}
-
-
-		//  Send Details To The User
-		auth := smtp.PlainAuth("", "tprop48@gmail.com", "ovgo agtz dsdj bwhq", "smtp.gmail.com")
-		to := []string{incBodyData.Email}
-		msgStr := fmt.Sprintf("To: %s\r\nSubject: Your Details\r\n\r\nID:%s \r\n Password:%s\r\n", incBodyData.Email, username,resPass)
-		msg := []byte(msgStr)
-	err = smtp.SendMail("smtp.gmail.com:587", auth, "tprop48@gmail.com", to, msg)
-		if err != nil {
-		log.Fatal(err)
-
-		}
-
-
 	} else {
+		json.NewEncoder(response).Encode(map[string]interface{}{
+			"Success": "false",
+			"Message": responeCreateUser["errorMessage"],
+		})
 		fmt.Println("Failed to create user")
 	}
 
